63323140704fca508962992519b5a8596f8d1089
MAvpacheater <mavpa171@gmail.com>
2025-09-30 21:05:57 +0300

    Update peoples.js

--- other/peoples/peoples.js
+++ other/peoples/peoples.js
@@ -10,7 +10,7 @@ async function loadPeoplesTranslations() {
     
     try {
         console.log('📥 Loading peoples translations...');
-        const response = await fetch('languages/peoples.json');
+        const response = await fetch('other/peoples/peoples.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
