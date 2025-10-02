9fd4ef9a443772b685c6a9b0e90886ab7b555bef
MAvpacheater <mavpa171@gmail.com>
2025-10-02 17:59:02 +0300

    Update help.js

--- other/help.js
+++ other/help.js
@@ -9,7 +9,7 @@ async function loadHelpTranslations() {
     
     try {
         console.log('📥 Loading help translations...');
-        const response = await fetch('other/help/help.json');
+        const response = await fetch('other/help.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
